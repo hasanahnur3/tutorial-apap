098e68de82e7489be4f19187baf99fa4a23218bb
hasanahnur3 <54942378+hasanahnur3@users.noreply.github.com>
2020-12-10 20:24:10 +0700

    Feat/tutorial 7 (#11)
    * feat: add fronted
    
    * docs: update readme
    
    Co-authored-by: Nur Fauziah Hasanah <hasanahnur@msani.net>

--- frontend/src/AppFunc.js
+++ frontend/src/AppFunc.js
@@ -16,6 +16,9 @@ function App() {
 // below is the return value of useState
 // [favItems, setFavItems] = [state, setState]
  const [favItems, setFavItems] = useState(() => []);
+ const [buttonHidden, setButtonHidden] = useState(true);
+ const [favoriteHidden, setFavoriteHidden] = useState(false);
+ 
  function handleItemClick(item) {
  // immutability
  const newItems = [...favItems];
@@ -23,33 +26,84 @@ function App() {
  // Find index of item with id
  const targetInd = newItems.findIndex(it => it.id === newItem.id);
  if (targetInd < 0) newItems.push(newItem);
- else newItems.splice(targetInd, 1); // delete 1 item at index targetInd
+//  else newItems.splice(targetInd, 1); // delete 1 item at index targetInd
  // trigger to set a new state
  setFavItems(newItems);
+ setButtonHidden(false);
  }
+
+ function handleItemClickRight(item) {
+    // immutability
+    const newItems = [...favItems];
+    const newItem = { ...item };
+    // Find index of item with id
+    const targetInd = newItems.findIndex(it => it.id === newItem.id);
+    if (targetInd < 0) newItems.push(newItem);
+    else newItems.splice(targetInd, 1); // delete 1 item at index targetInd
+    // trigger to set a new state
+    setFavItems(newItems);
+    setButtonHidden(false);
+}
+
+function deleteFavorite() {
+    setFavItems([]);
+    setButtonHidden(true);
+}
+
+function handleToggle() {
+    setFavoriteHidden(!favoriteHidden);
+  }
+
  return (
     <div className="container-fluid">
-    <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
-    <p className="text-center text-secondary text-sm font-italic">
-    (This is a <strong>function-based</strong> application)
-    </p>
-    <div className="container pt-3">
-    <div className="row">
-    <div className="col-sm">
-    <List
-    title="List Movies"
-    items={listMovies}
-    onItemClick={handleItemClick}
-    />
-    </div>
-    <div className="col-sm">
-    <List
-    title="My Favorites"
-    items={favItems}
-    onItemClick={handleItemClick}
-    />
-    </div>
-    </div>
+    <div className="container-fluid">
+      <h1 className="text-center mt-3 mb-0">Favorites Movie App</h1>
+            <p className="text-center text-secondary text-sm font-italic">
+                (This is a <strong>function-based</strong> application)
+            </p>
+      <div className="container pt-3">
+        <div className="row">
+          <div className="col-sm text-center">
+            <div className="form-check">
+              <input
+                type="checkbox"
+                checked={favoriteHidden ? true : false}
+                onChange={handleToggle}
+              />
+              <label>
+                Hide Favorites
+              </label>
+            </div>
+            <button
+              hidden={buttonHidden}
+              className="btn btn-primary"
+              onClick={deleteFavorite}
+            >
+              Clear Favorite
+            </button>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <List
+              title="List Movies"
+              items={listMovies}
+              onItemClick={handleItemClick}
+            />
+                    </div>
+                    <div className="col-sm">
+                    {!favoriteHidden ? (
+                        
+                        <List
+                            title="My Favorites"
+                            items={favItems}
+                            onItemClick={handleItemClickRight}
+                        ></List>
+                        
+                    ) : null}
+                    </div>
+                    </div>
+      </div>
     </div>
     </div>
     );
